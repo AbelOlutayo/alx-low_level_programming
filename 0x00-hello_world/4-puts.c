dd175273b8757594a72b1ba5fa14fa3d31c1acb7
AbelOlutayo <abelceler@gmail.com>
2022-04-07 21:34:35 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+     char str[100];
+
+     strcpy(str, "Programming is like building a multilingual puzzle");
+
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
