81360f80a3dd05ea979becde0c5383f3194e28c3
AbelOlutayo <abelceler@gmail.com>
2022-04-07 22:59:33 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - function begins
  *
  * return: function ends
  */
 
-int main(void)//
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
